ef792b45512c100c4f68af71215a30b957c055e2
Samuel Chrisdian <samuelchrisdian2003@gmail.com>
2025-07-20 13:09:46 +0700

    add seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,9 +6,6 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
- */
 class UserFactory extends Factory
 {
     /**
@@ -27,7 +24,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
+            'password' => static::$password ??= Hash::make('P@ssw0rd'),
             'remember_token' => Str::random(10),
         ];
     }
